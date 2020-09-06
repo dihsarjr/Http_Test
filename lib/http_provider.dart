a23fae523b55b091965983b28435ddc09192ca1d
Mohammed Rashid k <mohammed1rashid786@gmail.com>
2020-09-06 21:57:46 +0530

    c

--- lib/http_provider.dart
+++ lib/http_provider.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
+import 'package:http_test/model_post.dart';
 
-class HttpProvider with ChangeNotifier{
-  
+class HttpProvider with ChangeNotifier {
+  final url = 'https://jsonplaceholder.typicode.com/posts';
+
+  Future<List<Post>> getPost() async {}
 }
