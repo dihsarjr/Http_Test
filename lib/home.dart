fbb9ee3708a54218e4663e29558f63894b100cfd
Mohammed Rashid k <mohammed1rashid786@gmail.com>
2020-09-06 22:05:05 +0530

    reqDon

--- lib/home.dart
+++ lib/home.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:http_test/http_provider.dart';
+import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var httpProvider = Provider.of<HttpProvider>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Http Test'),
       ),
       body: Column(
-        children: [Container()],
+        children: [
+          Expanded(
+              child: Container(
+            child: Center(
+              child: RaisedButton(
+                onPressed: () {
+                  httpProvider.getPost();
+                },
+                child: Text('Get'),
+              ),
+            ),
+          ))
+        ],
       ),
     );
   }
--- lib/http_provider.dart
+++ lib/http_provider.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart';
 import 'package:http_test/model_post.dart';
 
 class HttpProvider with ChangeNotifier {
   final url = 'https://jsonplaceholder.typicode.com/posts';
 
-  Future<List<Post>> getPost() async {}
+  Future<List<Post>> getPost() async {
+    Response response = await get(url);
+
+    if (response.statusCode == 200) {
+      print(response.body);
+    }
+    notifyListeners();
+  }
 }
