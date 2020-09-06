63850693f71353a5392f2caf97c75f139e444370
Mohammed Rashid k <mohammed1rashid786@gmail.com>
2020-09-06 22:48:29 +0530

    done

--- lib/home.dart
+++ lib/home.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:http_test/http_provider.dart';
+import 'package:http_test/model_post.dart';
 import 'package:provider/provider.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<Post> data;
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     var httpProvider = Provider.of<HttpProvider>(context);
+    Provider.of<HttpProvider>(context).getPost().then((value) {
+      setState(() {
+        data = value;
+      });
+    });
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Http Test'),
@@ -14,14 +32,43 @@ class Home extends StatelessWidget {
         children: [
           Expanded(
               child: Container(
-            child: Center(
-              child: RaisedButton(
-                onPressed: () {
-                  httpProvider.getPost();
-                },
-                child: Text('Get'),
-              ),
-            ),
+            child: data == null
+                ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : ListView.builder(
+                    itemBuilder: (context, i) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: 200,
+                          width: 200,
+                          color: Colors.amber,
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(data[i].body),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(data[i].id.toString()),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(data[i].title),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(data[i].userId.toString()),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                    itemCount: data.length,
+                  ),
           ))
         ],
       ),
--- lib/http_provider.dart
+++ lib/http_provider.dart
@@ -1,16 +1,29 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart';
 import 'package:http_test/model_post.dart';
 
 class HttpProvider with ChangeNotifier {
   final url = 'https://jsonplaceholder.typicode.com/posts';
+  List<Post> posts = [];
 
   Future<List<Post>> getPost() async {
     Response response = await get(url);
 
     if (response.statusCode == 200) {
+      List<dynamic> body = jsonDecode(response.body);
+      posts = body.map((dynamic item) => Post.fromJson(item)).toList();
+
       print(response.body);
+      return posts;
+    } else {
+      // throw 'Something is Wrong';
+      print('Something wrong');
     }
+
     notifyListeners();
   }
+
+  notifyListeners();
 }
